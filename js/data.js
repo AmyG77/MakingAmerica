5d0fa9197a42986c38685e2b19b051daeb71e2b2
Amy S George <37734793+AmyG77@users.noreply.github.com>
2026-02-23 16:21:58 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const HistoricalData = {
+    optimizations: {
+        1830: { label: "Land Clearance", truth: "Forced removal of indigenous populations." },
+        1850: { label: "Labor Efficiency", truth: "Systemic utilization of chattel slavery / indentured labor." },
+        1930: { label: "Zoning Stabilization", truth: "Introduction of HOLC Redlining maps." },
+        1950: { label: "Infrastructure Growth", truth: "Strategic displacement of urban centers for highway expansion." }
+    }
+};
