e94a660b1f6a88b915d64cd6c4610e0efdbe768e
Amy S George <37734793+AmyG77@users.noreply.github.com>
2026-02-24 07:01:45 +0700

    Update ui-manager.js

--- js/ui-manager.js
+++ js/ui-manager.js
@@ -5,7 +5,7 @@ const ui = {
     },
 
     switchToModern() {
-        // Only trigger this once in 2026
+        // Switch control bars in 2026
         document.getElementById('build-controls').classList.add('hidden');
         document.getElementById('restore-controls').classList.remove('hidden');
         document.getElementById('npc-text').innerText = "The age of extraction is over. Will you give the land back?";
